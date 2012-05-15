3c93e787ac7a44048a03b46553ab63990dfc0b29
snowgoose.yk <snowgoose.yk@gmail.com>
2012-05-15 18:46:57 +0900

    extends MultiModule

--- src/main/java/org/analogweb/DirectionFormatter.java
+++ src/main/java/org/analogweb/DirectionFormatter.java
@@ -1,6 +1,6 @@
 package org.analogweb;
 
-public interface DirectionFormatter {
+public interface DirectionFormatter extends MultiModule {
 
     void formatAndWriteInto(RequestContext writeTo, String charset, Object source);
 
