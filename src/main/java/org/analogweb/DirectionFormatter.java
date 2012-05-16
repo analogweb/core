f1e67385992fcf1796c84a5549bd7b9e36fa37c9
snowgoose.yk <snowgoose.yk@gmail.com>
2012-05-17 05:29:26 +0900

    Add comment.

--- src/main/java/org/analogweb/DirectionFormatter.java
+++ src/main/java/org/analogweb/DirectionFormatter.java
@@ -1,7 +1,18 @@
 package org.analogweb;
 
+/**
+ * レスポンスボディに、特定のフォーマットをレンダリング可能な{@link Direction}における、
+ * フォーマット処理を定義します。
+ * @author snowgoose
+ */
 public interface DirectionFormatter extends MultiModule {
 
+    /**
+     * 指定されたオブジェクトをフォーマットし、レスポンスに書き込みを行います。
+     * @param writeTo 書き込みを行う{@link RequestContext}
+     * @param charset 書き込み時に使用される文字コード
+     * @param source フォーマットを行うオブジェクト
+     */
     void formatAndWriteInto(RequestContext writeTo, String charset, Object source);
 
 }
--- src/main/java/org/analogweb/DirectionFormatterAware.java
+++ src/main/java/org/analogweb/DirectionFormatterAware.java
@@ -1,10 +1,17 @@
 package org.analogweb;
 
 /**
+ * {@link DirectionFormatter}が適用可能な{@link Direction}である事を
+ * 表します。
  * @author snowgoose
  */
 public interface DirectionFormatterAware<T extends Direction> extends Direction {
     
+    /**
+     * {@link DirectionFormatter}を適用します。
+     * @param formatter {@link DirectionFormatter}
+     * @return フォーマッタが適用された{@link Direction}の自身のインスタンス
+     */
     T attach(DirectionFormatter formatter);
 
 }
