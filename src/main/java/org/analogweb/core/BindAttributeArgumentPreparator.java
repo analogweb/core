92936e766887b9b12ae8588be8c5ef6ea8cb6c5c
snowgoose.yk <snowgoose.yk@gmail.com>
2012-06-26 18:44:58 +0900

    Extract method

--- src/main/java/org/analogweb/core/BindAttributeArgumentPreparator.java
+++ src/main/java/org/analogweb/core/BindAttributeArgumentPreparator.java
@@ -17,8 +17,8 @@ import org.analogweb.util.AnnotationUtils;
 import org.analogweb.util.StringUtils;
 
 /**
- * アクションメソッドの引数から{@link As}が付与されている引数を検知し、 {@link As}
- * に定義されたスコープと属性名に応じた値をアクションメソッド の引数の値として設定する
+ * エントリポイントメソッドの引数から{@link As}が付与されている引数を検知し、 {@link As}
+ * に定義されたスコープと属性名に応じた値をエントリポイントメソッド の引数の値として設定する
  * {@link AbstractInvocationProcessor}の実装です。
  * @author snowgoose
  */
@@ -31,34 +31,39 @@ public class BindAttributeArgumentPreparator extends AbstractInvocationProcessor
         Annotation[][] parameterAnnotations = method.getParameterAnnotations();
         Class<?>[] argTypes = metadata.getArgumentTypes();
         for (int index = 0, limit = argTypes.length; index < limit; index++) {
-            Class<?> argType = argTypes[index];
-            As bindAttribute = AnnotationUtils
-                    .findAnnotation(As.class, parameterAnnotations[index]);
-            if (bindAttribute != null) {
-                Scope scope = AnnotationUtils.findAnnotation(Scope.class,
-                        parameterAnnotations[index]);
-                String scopeValue = StringUtils.EMPTY;
-                if (scope != null) {
-                    scopeValue = scope.value();
-                }
-                Object value = attributes.getValueOfQuery(context, scopeValue,
-                        bindAttribute.value());
-                if (value != null) {
-                    MapWith mapWith = AnnotationUtils.findAnnotation(MapWith.class,
-                            parameterAnnotations[index]);
-                    Class<? extends TypeMapper> mapperType = TypeMapper.class;
-                    if (mapWith != null) {
-                        mapperType = mapWith.value();
-                    }
-                    Formats f = AnnotationUtils.findAnnotation(Formats.class,
-                            parameterAnnotations[index]);
-                    Object convertedValue = converters.mapToType(mapperType, context, attributes,
-                            value, argType, (f != null) ? f.value() : new String[0]);
-                    invocation.putPreparedArg(index, convertedValue);
-                }
+            Object convertedValue = convert(attributes, context, converters, argTypes[index],
+                    parameterAnnotations[index]);
+            if (convertedValue != null) {
+                invocation.putPreparedArg(index, convertedValue);
             }
         }
         return invocation;
+    }
+
+    protected Object convert(RequestAttributes attributes, RequestContext context,
+            TypeMapperContext converters, Class<?> argType, Annotation[] parameterAnnotations) {
+        As bindAttribute = AnnotationUtils.findAnnotation(As.class, parameterAnnotations);
+        if (bindAttribute != null) {
+            Scope scope = AnnotationUtils.findAnnotation(Scope.class, parameterAnnotations);
+            String scopeValue = StringUtils.EMPTY;
+            if (scope != null) {
+                scopeValue = scope.value();
+            }
+            Object value = attributes.getValueOfQuery(context, scopeValue, bindAttribute.value());
+            if (value != null) {
+                MapWith mapWith = AnnotationUtils.findAnnotation(MapWith.class,
+                        parameterAnnotations);
+                Class<? extends TypeMapper> mapperType = TypeMapper.class;
+                if (mapWith != null) {
+                    mapperType = mapWith.value();
+                }
+                Formats f = AnnotationUtils.findAnnotation(Formats.class, parameterAnnotations);
+                Object convertedValue = converters.mapToType(mapperType, context, attributes,
+                        value, argType, (f != null) ? f.value() : new String[0]);
+                return convertedValue;
+            }
+        }
+        return null;
     }
 
 }
