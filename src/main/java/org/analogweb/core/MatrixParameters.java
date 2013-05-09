ec29ea9754e93de4aaff30e28f04604583e3c9e8
snowgooseyk <snowgoose.yk@gmail.com>
2013-05-10 06:24:24 +0900

    Add MatrixParameters #34

---
+++ src/main/java/org/analogweb/core/MatrixParameters.java
@@ -0,0 +1,41 @@
+package org.analogweb.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.Charset;
+
+import org.analogweb.MediaType;
+import org.analogweb.util.StringUtils;
+
+/**
+ * @author snowgooseyk
+ */
+public class MatrixParameters extends QueryParameters {
+
+    public MatrixParameters(URI requestURI) {
+        super(requestURI);
+    }
+
+    @Override
+    protected String resolveParametersParts(URI requestURI, InputStream body,
+            MediaType contentType, Charset charset) throws IOException {
+        String path = requestURI.getRawQuery();
+        if (StringUtils.isEmpty(path)) {
+            path = requestURI.getRawPath();
+        } else {
+            path = requestURI.getRawPath() + '?' + path;
+        }
+        int splitted = path.indexOf(';');
+        if (splitted < 0) {
+            return StringUtils.EMPTY;
+        } else {
+            return StringUtils.substring(path, splitted + 1);
+        }
+    }
+
+    @Override
+    protected char getParameterSeparator() {
+        return ';';
+    }
+}
---
+++ src/test/java/org/analogweb/core/MatrixParametersTest.java
@@ -0,0 +1,47 @@
+package org.analogweb.core;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.net.URI;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MatrixParametersTest {
+
+    private MatrixParameters params;
+
+    @Test
+    public void test() {
+        URI requestURI = URI.create("http://some.where/thing;paramA=1;paramB=6542");
+        params = new MatrixParameters(requestURI);
+        List<String> actual = params.getValues("paramA");
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get(0), is("1"));
+        actual = params.getValues("paramB");
+        assertThat(actual.size(), is(1));
+        assertThat(actual.get(0), is("6542"));
+    }
+
+    @Test
+    public void testContainsPath() {
+        URI requestURI = URI
+                .create("http://example.com/res/categories;name=foo/objects;name=green/?page=1");
+        params = new MatrixParameters(requestURI);
+        List<String> actual = params.getValues("name");
+        assertThat(actual.size(), is(2));
+        assertThat(actual.get(0), is("foo/objects"));
+        assertThat(actual.get(1), is("green/?page=1"));
+    }
+
+    @Test
+    public void testNoMatrixParameters() {
+        URI requestURI = URI.create("http://some.where/thing?paramA=1&paramB=6542");
+        params = new MatrixParameters(requestURI);
+        List<String> actual = params.getValues("paramA");
+        assertThat(actual.size(), is(0));
+        actual = params.getValues("paramB");
+        assertThat(actual.size(), is(0));
+    }
+}
