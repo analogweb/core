dfdcfd933b9d93ce1b38da57412b31b419f5ef9f
snowgooseyk <snowgoose.yk@gmail.com>
2013-09-03 05:43:25 +0900

    Remve Testcase

--- src/test/java/org/analogweb/core/DefaultRequestPathTest.java
+++ src/test/java/org/analogweb/core/DefaultRequestPathTest.java
@@ -66,27 +66,6 @@ public class DefaultRequestPathTest {
     }
 
     @Test
-    public void testPathThrowgh() throws Exception {
-
-        URI uri = new URI(
-                "http://somehost:8080/foo/baa.do;jsessionid=1A26E401D812045AF2D9150891DA01B3");
-        URI baseUri = new URI("http://somehost:8080/foo");
-
-        DefaultRequestPath actual = new DefaultRequestPath(baseUri, uri, "POST");
-//        assertTrue(actual.pathThrowgh(""));
-    }
-
-    @Test
-    public void testPathThrowghAndRequestMethodLowerCase() throws Exception {
-        URI uri = new URI(
-                "http://somehost:8080/foo/baa.do;jsessionid=1A26E401D812045AF2D9150891DA01B3");
-        URI baseUri = new URI("http://somehost:8080/foo");
-
-        DefaultRequestPath actual = new DefaultRequestPath(baseUri, uri, "Post");
-//        assertFalse(actual.pathThrowgh(".do"));
-    }
-
-    @Test
     public void testNotPathThrowghWithContextRootPath() throws Exception {
         URI uri = new URI("http://somehost:8080/baa;jsessionid=1A26E401D812045AF2D9150891DA01B3");
 
