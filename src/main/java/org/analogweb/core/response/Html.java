fc446b24033880cc4f7db114294f65174323e475
snowgooseyk <snowgoose.yk@gmail.com>
2014-01-06 14:54:10 +0900

    Add Constractor

--- src/main/java/org/analogweb/core/response/Html.java
+++ src/main/java/org/analogweb/core/response/Html.java
@@ -4,58 +4,69 @@ import java.util.Map;
 
 import org.analogweb.ResponseFormatter;
 import org.analogweb.util.Assertion;
+import org.analogweb.util.Maps;
 
 /**
  * @author snowgoose
  */
 public class Html extends TextFormattable<Html> {
 
-    private static final String DEFAULT_HTML_CHARSET = "UTF-8";
-    private static final String DEFAULT_HTML_CONTENT_TYPE = "text/html";
+	private static final String DEFAULT_HTML_CHARSET = "UTF-8";
+	private static final String DEFAULT_HTML_CONTENT_TYPE = "text/html";
 
-    public static Html as(String templatePath,Map<String,Object> context) {
-        Html html = new Html(new HtmlTemplate(templatePath, context));
-        return html;
-    }
+	public static Html as(String templatePath) {
+		Html html = new Html(new HtmlTemplate(templatePath,
+				Maps.<String, Object> newEmptyHashMap()));
+		return html;
+	}
 
-    public static class HtmlTemplate {
-        private String templateResourcePath;
-        private Map<String,Object> context;
-        public HtmlTemplate(String templateResourcePath,Map<String,Object> context){
-            Assertion.notNull(templateResourcePath, "Template resource path");
-            this.templateResourcePath = templateResourcePath;
-            this.context = context;
-        }
-        public String getTemplateResource() {
-            return templateResourcePath;
-        }
-        public Map<String, Object> getContext() {
-            return context;
-        }
-    }
+	public static Html as(String templatePath, Map<String, Object> context) {
+		Html html = new Html(new HtmlTemplate(templatePath, context));
+		return html;
+	}
 
-    @SuppressWarnings("unchecked")
-    public static Html with(String str) {
-        Html html = new Html(str);
-        return html;
-    }
+	public static class HtmlTemplate {
+		private String templateResourcePath;
+		private Map<String, Object> context;
 
-    protected Html(Object source) {
-        super(source);
-        super.typeAs(DEFAULT_HTML_CONTENT_TYPE);
-        super.withCharset(DEFAULT_HTML_CHARSET);
-    }
+		public HtmlTemplate(String templateResourcePath,
+				Map<String, Object> context) {
+			Assertion.notNull(templateResourcePath, "Template resource path");
+			this.templateResourcePath = templateResourcePath;
+			this.context = context;
+		}
 
-    protected Html(String input) {
-        super(input);
-        super.typeAs(DEFAULT_HTML_CONTENT_TYPE);
-        super.withCharset(DEFAULT_HTML_CHARSET);
-    }
+		public String getTemplateResource() {
+			return templateResourcePath;
+		}
 
-    @Override
-    protected ResponseFormatter getDefaultFormatter() {
-        // TODO Implement!
-        throw new UnsupportedOperationException();
-    }
+		public Map<String, Object> getContext() {
+			return context;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Html with(String str) {
+		Html html = new Html(str);
+		return html;
+	}
+
+	protected Html(Object source) {
+		super(source);
+		super.typeAs(DEFAULT_HTML_CONTENT_TYPE);
+		super.withCharset(DEFAULT_HTML_CHARSET);
+	}
+
+	protected Html(String input) {
+		super(input);
+		super.typeAs(DEFAULT_HTML_CONTENT_TYPE);
+		super.withCharset(DEFAULT_HTML_CHARSET);
+	}
+
+	@Override
+	protected ResponseFormatter getDefaultFormatter() {
+		// TODO Implement!
+		throw new UnsupportedOperationException();
+	}
 
 }
