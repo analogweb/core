ac8f084b386e11339393f970532c010be965380f
snowgooseyk <snowgoose.yk@gmail.com>
2014-01-06 15:10:11 +0900

    Add Acceptable to BasicResponses

--- src/main/java/org/analogweb/core/response/BasicResponses.java
+++ src/main/java/org/analogweb/core/response/BasicResponses.java
@@ -6,48 +6,56 @@ import java.util.Map;
  * @author snowgooseyk
  */
 public final class BasicResponses {
-    
-    private BasicResponses(){
-        // nop.
-    }
-    
-    public static Text text(String text){
-        return Text.with(text);
-    }
-    
-    public static Html html(String text){
-        return Html.with(text);
-    }
 
-    public static Html html(String template,Map<String, Object> parameters){
-        return Html.as(template, parameters);
-    }
-    
-    public static Redirect redirect(String to){
-        return Redirect.to(to);
-    }
+	private BasicResponses() {
+		// nop.
+	}
 
-    public static Json json(String json){
-        return Json.with(json);
-    }
+	public static Acceptable acceptable(Object object) {
+		return Acceptable.as(object);
+	}
 
-    public static Json json(Object obj){
-        return Json.as(obj);
-    }
+	public static Text text(String text) {
+		return Text.with(text);
+	}
 
-    public static Xml xml(String xml){
-        return Xml.with(xml);
-    }
+	public static Html htmlText(String text) {
+		return Html.with(text);
+	}
 
-    public static Xml xml(Object obj){
-        return Xml.as(obj);
-    }
+	public static Html html(String template, Map<String, Object> parameters) {
+		return Html.as(template, parameters);
+	}
 
-    public static HttpStatus status(int statusCode){
-        return HttpStatus.valueOf(statusCode);
-    }
+	public static Html html(String template) {
+		return Html.as(template);
+	}
 
-    public static HttpStatus status(String status){
-        return HttpStatus.valueOf(status);
-    }
+	public static Redirect redirect(String to) {
+		return Redirect.to(to);
+	}
+
+	public static Json json(String json) {
+		return Json.with(json);
+	}
+
+	public static Json json(Object obj) {
+		return Json.as(obj);
+	}
+
+	public static Xml xml(String xml) {
+		return Xml.with(xml);
+	}
+
+	public static Xml xml(Object obj) {
+		return Xml.as(obj);
+	}
+
+	public static HttpStatus status(int statusCode) {
+		return HttpStatus.valueOf(statusCode);
+	}
+
+	public static HttpStatus status(String status) {
+		return HttpStatus.valueOf(status);
+	}
 }
