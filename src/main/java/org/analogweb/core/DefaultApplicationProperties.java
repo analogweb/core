470e841dc4aabcd8fd56861bdd4a01e44794121a
snowgooseyk <snowgoose.yk@gmail.com>
2014-12-07 01:13:50 +0900

    Append application specifier to temporaly directory name.

--- src/main/java/org/analogweb/core/DefaultApplicationProperties.java
+++ src/main/java/org/analogweb/core/DefaultApplicationProperties.java
@@ -19,92 +19,90 @@ import org.analogweb.util.SystemProperties;
  */
 public class DefaultApplicationProperties implements ApplicationProperties {
 
-	private final Collection<String> packageNames;
-	private final String tempDirectoryPath;
-	private final Locale defaultClientLocale;
+    private final Collection<String> packageNames;
+    private final String tempDirectoryPath;
+    private final Locale defaultClientLocale;
 
-	public static DefaultApplicationProperties defaultProperties() {
-		return new DefaultApplicationProperties();
-	}
+    public static DefaultApplicationProperties defaultProperties() {
+        return new DefaultApplicationProperties();
+    }
 
-	public static DefaultApplicationProperties properties(String packageNames) {
-		return properties(packageNames, null, null);
-	}
+    public static DefaultApplicationProperties properties(String packageNames) {
+        return properties(packageNames, null, null);
+    }
 
-	public static DefaultApplicationProperties properties(String packageNames,
-			String tmpDirectoryPath, String defaultClientLocale) {
-		return new DefaultApplicationProperties(packageNames, tmpDirectoryPath,
-				defaultClientLocale);
-	}
+    public static DefaultApplicationProperties properties(String packageNames,
+            String tmpDirectoryPath, String defaultClientLocale) {
+        return new DefaultApplicationProperties(packageNames, tmpDirectoryPath, defaultClientLocale);
+    }
 
-	protected DefaultApplicationProperties() {
-		this(Application.class.getPackage().getName(), null, null);
-	}
+    protected DefaultApplicationProperties() {
+        this(Application.class.getPackage().getName(), null, null);
+    }
 
-	protected DefaultApplicationProperties(String packageNames,
-			String tempDirectoryPath, String defaultClientLocale) {
-		this.packageNames = createUserDefinedPackageNames(packageNames);
-		this.tempDirectoryPath = createTempDirPath(tempDirectoryPath);
-		this.defaultClientLocale = createDefaultClientLocale(defaultClientLocale);
-	}
+    protected DefaultApplicationProperties(String packageNames, String tempDirectoryPath,
+            String defaultClientLocale) {
+        this.packageNames = createUserDefinedPackageNames(packageNames);
+        this.tempDirectoryPath = createTempDirPath(tempDirectoryPath);
+        this.defaultClientLocale = createDefaultClientLocale(defaultClientLocale);
+    }
 
-	@Override
-	public File getTempDir() {
-		return new File(tempDirectoryPath);
-	}
+    @Override
+    public File getTempDir() {
+        return new File(tempDirectoryPath);
+    }
 
-	@Override
-	public Collection<String> getComponentPackageNames() {
-		return packageNames;
-	}
+    @Override
+    public Collection<String> getComponentPackageNames() {
+        return packageNames;
+    }
 
-	@Override
-	public Locale getDefaultClientLocale() {
-		return defaultClientLocale;
-	}
+    @Override
+    public Locale getDefaultClientLocale() {
+        return defaultClientLocale;
+    }
 
-	protected Locale createDefaultClientLocale(String locale) {
-		if (StringUtils.isEmpty(locale)) {
-			return Locale.getDefault();
-		}
-		List<String> values = StringUtils.split(locale.replace('_', '-'), '-');
-		switch (values.size()) {
-		case 2:
-			return new Locale(values.get(0), values.get(1));
-		case 3:
-			return new Locale(values.get(0), values.get(1), values.get(2));
-		default:
-			return new Locale(values.get(0));
-		}
-	}
+    protected Locale createDefaultClientLocale(String locale) {
+        if (StringUtils.isEmpty(locale)) {
+            return Locale.getDefault();
+        }
+        List<String> values = StringUtils.split(locale.replace('_', '-'), '-');
+        switch (values.size()) {
+        case 2:
+            return new Locale(values.get(0), values.get(1));
+        case 3:
+            return new Locale(values.get(0), values.get(1), values.get(2));
+        default:
+            return new Locale(values.get(0));
+        }
+    }
 
-	protected Set<String> createUserDefinedPackageNames(
-			String tokenizedRootPackageNames) {
-		Set<String> packageNames = new HashSet<String>();
-		if (StringUtils.isNotEmpty(tokenizedRootPackageNames)) {
-			for (String packageName : StringUtils.split(
-					tokenizedRootPackageNames, ',')) {
-				packageNames.add(packageName);
-			}
-		}
-		return Collections.unmodifiableSet(packageNames);
-	}
+    protected Set<String> createUserDefinedPackageNames(String tokenizedRootPackageNames) {
+        Set<String> packageNames = new HashSet<String>();
+        if (StringUtils.isNotEmpty(tokenizedRootPackageNames)) {
+            for (String packageName : StringUtils.split(tokenizedRootPackageNames, ',')) {
+                packageNames.add(packageName);
+            }
+        }
+        return Collections.unmodifiableSet(packageNames);
+    }
 
-	protected String createTempDirPath(String tmpDirPath) {
-		String tmpDir = tmpDirPath;
-		if (StringUtils.isEmpty(tmpDir)) {
-			tmpDir = SystemProperties.tmpDir();
-		}
-		if (tmpDir.endsWith(SystemProperties.fileSeparator())) {
-			tmpDir = tmpDir + Application.class.getCanonicalName();
-		} else {
-			tmpDir = tmpDir + SystemProperties.fileSeparator()
-					+ Application.class.getCanonicalName();
-		}
-		String path = tmpDir + SystemProperties.fileSeparator() + UUID.randomUUID();
-		while (new File(path).exists()) {
-			path = tmpDir + SystemProperties.fileSeparator() + UUID.randomUUID();
-		}
-		return path;
-	}
+    protected String createTempDirPath(String tmpDirPath) {
+        String tmpDir = tmpDirPath;
+        if (StringUtils.isEmpty(tmpDir)) {
+            tmpDir = SystemProperties.tmpDir() + SystemProperties.fileSeparator()
+                    + Application.class.getCanonicalName();
+        }
+        if (tmpDir.endsWith(SystemProperties.fileSeparator())) {
+            tmpDir = tmpDir + Application.class.getCanonicalName();
+        } else {
+            tmpDir = tmpDir + SystemProperties.fileSeparator()
+                    + Application.class.getCanonicalName();
+        }
+        String path = tmpDir + SystemProperties.fileSeparator() + UUID.randomUUID();
+        while (new File(path).exists()) {
+            path = tmpDir + SystemProperties.fileSeparator() + UUID.randomUUID();
+        }
+        return path;
+    }
 }
