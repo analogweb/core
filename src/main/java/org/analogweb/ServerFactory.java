0b4f009e883d49dd6db29f01eab090e2a576cfe2
snowgooseyk <snowgoose.yk@gmail.com>
2015-01-22 23:22:00 +0900

    Add ServerFactory

---
+++ src/main/java/org/analogweb/ServerFactory.java
@@ -0,0 +1,12 @@
+package org.analogweb;
+
+import java.net.URI;
+
+/**
+ * @author snowgooseyk
+ */
+public interface ServerFactory {
+
+    Server create(URI uri, ApplicationProperties properties, ApplicationContext context,
+            Application application);
+}
