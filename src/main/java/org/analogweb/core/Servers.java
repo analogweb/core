4058309fca4895cd0881fad3a5c52d132cebd0af
snowgooseyk <snowgoose.yk@gmail.com>
2015-01-23 01:09:13 +0900

    Add log message.

--- src/main/java/org/analogweb/core/Servers.java
+++ src/main/java/org/analogweb/core/Servers.java
@@ -13,11 +13,16 @@ import org.analogweb.util.ClassUtils;
 import org.analogweb.util.Maps;
 import org.analogweb.util.ReflectionUtils;
 import org.analogweb.util.StringUtils;
+import org.analogweb.util.logging.Log;
+import org.analogweb.util.logging.Logs;
+import org.analogweb.util.logging.Markers;
 
 /**
  * @author snowgooseyk
  */
 public final class Servers {
+
+    private static final Log log = Logs.getLog(Servers.class);
 
     private Servers() {
         // nop.
@@ -41,12 +46,14 @@ public final class Servers {
 
     public static Server create(URI uri, ApplicationProperties properties,
             ApplicationContext context, Application application) {
+        log.log(Markers.BOOT_APPLICATION, "IB000005");
         Class<?> probablyFactoryClass = ClassUtils.forNameQuietly(ServerFactory.class.getPackage()
                 .getName() + ".ServerFactoryImpl");
         if (probablyFactoryClass != null) {
             Object probablyFactory = ReflectionUtils.getInstanceQuietly(probablyFactoryClass);
             if (probablyFactory instanceof ServerFactory) {
                 ServerFactory factory = (ServerFactory) probablyFactory;
+                log.log(Markers.BOOT_APPLICATION, "IB000006", factory.toString());
                 return factory.create(uri, properties, context, application);
             }
         }
@@ -55,6 +62,7 @@ public final class Servers {
 
     private static Server createDefaultServer(URI uri, ApplicationProperties properties,
             ApplicationContext context, Application application) {
+        log.log(Markers.BOOT_APPLICATION, "IB000007", "Sun HTTP Server");
         return HttpServers.create(uri, new AnalogHandler(application, context, properties));
     }
 }
