9272dd6c8344364275cedececadfed2cb13df8b0
snowgooseyk <snowgoose.yk@gmail.com>
2015-05-27 23:26:23 +0900

    HTTP GET NOT retrive form parameter. #70

--- src/main/java/org/analogweb/core/ParameterValueResolver.java
+++ src/main/java/org/analogweb/core/ParameterValueResolver.java
@@ -9,31 +9,33 @@ import org.analogweb.RequestValueResolver;
 import org.analogweb.util.StringUtils;
 
 /**
+ * Resolve parameter value via query parameter, matrix parameter and form parameter. 
  * @author snowgoose
  */
 public class ParameterValueResolver implements RequestValueResolver {
 
-	@Override
-	public Object resolveValue(RequestContext requestContext,
-			InvocationMetadata metadata, String name, Class<?> requiredType,Annotation[] annotations) {
-		if (StringUtils.isEmpty(name)) {
-			return null;
-		}
-		List<String> values = requestContext.getQueryParameters().getValues(
-				name);
-		if (values == null || values.isEmpty()) {
-			values = requestContext.getMatrixParameters().getValues(name);
-			if (values == null || values.isEmpty()) {
-				values = requestContext.getFormParameters().getValues(name);
-				if (values == null || values.isEmpty()) {
-					return null;
-				}
-			}
-		}
-		if (String[].class.equals(requiredType)) {
-			return (values.isEmpty()) ? null : values.toArray(new String[values
-					.size()]);
-		}
-		return values.get(0);
-	}
+    @Override
+    public Object resolveValue(RequestContext requestContext, InvocationMetadata metadata,
+            String name, Class<?> requiredType, Annotation[] annotations) {
+        if (StringUtils.isEmpty(name)) {
+            return null;
+        }
+        List<String> values = requestContext.getQueryParameters().getValues(name);
+        if (values == null || values.isEmpty()) {
+            values = requestContext.getMatrixParameters().getValues(name);
+            if (values == null || values.isEmpty()) {
+                if (requestContext.getRequestMethod().equalsIgnoreCase("GET")) {
+                    return null;
+                }
+                values = requestContext.getFormParameters().getValues(name);
+                if (values == null || values.isEmpty()) {
+                    return null;
+                }
+            }
+        }
+        if (String[].class.equals(requiredType)) {
+            return (values.isEmpty()) ? null : values.toArray(new String[values.size()]);
+        }
+        return values.get(0);
+    }
 }
--- src/test/java/org/analogweb/core/ParameterValueResolverTest.java
+++ src/test/java/org/analogweb/core/ParameterValueResolverTest.java
@@ -1,9 +1,8 @@
 package org.analogweb.core;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -20,80 +19,90 @@ import org.junit.Test;
  */
 public class ParameterValueResolverTest {
 
-	private ParameterValueResolver resolver;
-	private RequestContext requestContext;
-	private InvocationMetadata metadata;
-	private Parameters params;
-	private Parameters matrixParams;
+    private ParameterValueResolver resolver;
+    private RequestContext requestContext;
+    private InvocationMetadata metadata;
+    private Parameters params;
+    private Parameters matrixParams;
 
-	@Before
-	public void setUp() throws Exception {
-		resolver = new ParameterValueResolver();
-		requestContext = mock(RequestContext.class);
-		metadata = mock(InvocationMetadata.class);
-		params = mock(Parameters.class);
-		matrixParams = mock(Parameters.class);
-	}
+    @Before
+    public void setUp() throws Exception {
+        resolver = new ParameterValueResolver();
+        requestContext = mock(RequestContext.class);
+        metadata = mock(InvocationMetadata.class);
+        params = mock(Parameters.class);
+        matrixParams = mock(Parameters.class);
+    }
 
-	@Test
-	public void testResolveAttributeValue() {
-		when(requestContext.getQueryParameters()).thenReturn(params);
-		when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
-		Object actual = resolver.resolveValue(requestContext, metadata, "foo",
-				String.class, null);
-		assertThat(actual.toString(), is("baa"));
-	}
+    @Test
+    public void testResolveAttributeValue() {
+        when(requestContext.getQueryParameters()).thenReturn(params);
+        when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
+        Object actual = resolver.resolveValue(requestContext, metadata, "foo", String.class, null);
+        assertThat(actual.toString(), is("baa"));
+    }
 
-	@Test
-	public void testResolveAttributeValueViaFormParameters() {
-		Parameters empty = mock(Parameters.class);
-		when(requestContext.getQueryParameters()).thenReturn(empty);
-		when(requestContext.getMatrixParameters()).thenReturn(empty);
-		when(requestContext.getFormParameters()).thenReturn(params);
-		when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
-		Object actual = resolver.resolveValue(requestContext, metadata, "foo",
-				String.class, null);
-		assertThat(actual.toString(), is("baa"));
-	}
+    @Test
+    public void testResolveAttributeValueViaFormParameters() {
+        Parameters empty = mock(Parameters.class);
+        when(requestContext.getQueryParameters()).thenReturn(empty);
+        when(requestContext.getMatrixParameters()).thenReturn(empty);
+        when(requestContext.getFormParameters()).thenReturn(params);
+        when(requestContext.getRequestMethod()).thenReturn("POST");
+        when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
+        Object actual = resolver.resolveValue(requestContext, metadata, "foo", String.class, null);
+        assertThat(actual.toString(), is("baa"));
+    }
 
-	@Test
-	public void testResolveAttributeValueWithNullName() {
-		Object actual = resolver.resolveValue(requestContext, metadata, null,
-				String[].class, null);
-		assertNull(actual);
-	}
+    @Test
+    public void testNotResolveAttributeValueViaFormParameters() {
+        Parameters empty = mock(Parameters.class);
+        when(requestContext.getQueryParameters()).thenReturn(empty);
+        when(requestContext.getMatrixParameters()).thenReturn(empty);
+        when(requestContext.getFormParameters()).thenReturn(params);
+        when(requestContext.getRequestMethod()).thenReturn("GET");
+        when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
+        Object actual = resolver.resolveValue(requestContext, metadata, "foo", String.class, null);
+        assertThat(actual, is(nullValue()));
+    }
 
-	@Test
-	public void testResolveAttributeValueOfParameterArray() {
-		when(requestContext.getQueryParameters()).thenReturn(params);
-		when(params.getValues("foo")).thenReturn(Arrays.asList("baa", "baz"));
-		Object actual = resolver.resolveValue(requestContext, metadata, "foo",
-				String[].class, null);
-		assertTrue(actual instanceof String[]);
-		String[] actualArray = (String[]) actual;
-		assertThat(actualArray[0], is("baa"));
-		assertThat(actualArray[1], is("baz"));
-	}
+    @Test
+    public void testResolveAttributeValueWithNullName() {
+        Object actual = resolver.resolveValue(requestContext, metadata, null, String[].class, null);
+        assertThat(actual, is(nullValue()));
+    }
 
-	@Test
-	public void testResolveAttributeNoParameterValue() {
-		when(requestContext.getQueryParameters()).thenReturn(params);
-		when(requestContext.getMatrixParameters()).thenReturn(params);
-		when(requestContext.getFormParameters()).thenReturn(params);
-		when(params.getValues("foo")).thenReturn(null);
-		Object actual = resolver.resolveValue(requestContext, metadata, "foo",
-				String[].class, null);
-		assertNull(actual);
-	}
+    @Test
+    public void testResolveAttributeValueOfParameterArray() {
+        when(requestContext.getQueryParameters()).thenReturn(params);
+        when(params.getValues("foo")).thenReturn(Arrays.asList("baa", "baz"));
+        Object actual = resolver
+                .resolveValue(requestContext, metadata, "foo", String[].class, null);
+        String[] actualArray = (String[]) actual;
+        assertThat(actualArray[0], is("baa"));
+        assertThat(actualArray[1], is("baz"));
+    }
 
-	@Test
-	public void testResolveAttributeViaMatrixParam() {
-		when(requestContext.getQueryParameters()).thenReturn(params);
-		when(requestContext.getMatrixParameters()).thenReturn(matrixParams);
-		when(params.getValues("foo")).thenReturn(null);
-		when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
-		Object actual = resolver.resolveValue(requestContext, metadata, "foo",
-				String[].class, null);
-		assertThat(((String[]) actual)[0], is("baa"));
-	}
+    @Test
+    public void testResolveAttributeNoParameterValue() {
+        when(requestContext.getQueryParameters()).thenReturn(params);
+        when(requestContext.getMatrixParameters()).thenReturn(params);
+        when(requestContext.getFormParameters()).thenReturn(params);
+        when(requestContext.getRequestMethod()).thenReturn("POST");
+        when(params.getValues("foo")).thenReturn(null);
+        Object actual = resolver
+                .resolveValue(requestContext, metadata, "foo", String[].class, null);
+        assertThat(actual, is(nullValue()));
+    }
+
+    @Test
+    public void testResolveAttributeViaMatrixParam() {
+        when(requestContext.getQueryParameters()).thenReturn(params);
+        when(requestContext.getMatrixParameters()).thenReturn(matrixParams);
+        when(params.getValues("foo")).thenReturn(null);
+        when(params.getValues("foo")).thenReturn(Arrays.asList("baa"));
+        Object actual = resolver
+                .resolveValue(requestContext, metadata, "foo", String[].class, null);
+        assertThat(((String[]) actual)[0], is("baa"));
+    }
 }
